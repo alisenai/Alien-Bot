60010a18c0807ab8b8f1e02ab2e06b7c7f5d743c
Alien <neptunesblade@gmail.com>
2018-07-22 00:50:12 -0700

    delete-success-reply, delete-failure-reply, list-success-reply, list-failure-reply, set-deduction, and set-payout commands implemented for the Economy mod.

--- Mods/Economy/Economy.py
+++ Mods/Economy/Economy.py
@@ -1,3 +1,7 @@
+import re
+
+import discord
+
 import Common.DataManager as DataManager
 from Common import Utils
 from Common.Mod import Mod
@@ -45,6 +49,10 @@ class Economy(Mod):
             else:
                 await Utils.simple_embed_reply(message.channel, "[Error]",
                                                "Starting balance command parameter not supplied.")
+        elif command is self.commands["Set Payout Command"]:
+            await self.set_income_min_max(message, is_payout=True)
+        elif command is self.commands["Set Deduction Command"]:
+            await self.set_income_min_max(message, is_payout=False)
         elif command is self.commands["Balance Command"]:
             user_balance = self.database.execute("SELECT balance FROM '" + server.id + "' WHERE user='" +
                                                  author.id + "'LIMIT 1")[0]
@@ -86,20 +94,36 @@ class Economy(Mod):
             await self.set_income_reply(message, is_success=True)
         elif command is self.commands["Add Failure Reply Command"]:
             await self.set_income_reply(message, is_success=False)
+        elif command is self.commands["List Success Reply Command"]:
+            await self.list_reply_commands(message, is_success=True)
+        elif command is self.commands["List Failure Reply Command"]:
+            await self.list_reply_commands(message, is_success=False)
+        elif command is self.commands["Delete Success Reply Command"]:
+            await self.delete_command_reply(message, True)
+        elif command in self.commands["Delete Failure Reply Command"]:
+            await self.delete_command_reply(message, False)
 
+    # TODO: Add max reply message count and length limits
     async def set_income_reply(self, message, is_success):
+        server, channel, author = message.server, message.channel, message.author
         split_message = message.content.split(" ")
         if len(split_message) > 2:
             income_command = split_message[1]
-            success_reply = message.content[len(split_message[0]) + len(split_message[1]) + 2:]
+            reply = message.content[len(split_message[0]) + len(split_message[1]) + 2:]
             if income_command == "slut" or "work" or "crime":
                 income_command = "Slut Command" if income_command == "slut" else "Work Command" if income_command == "work" else "Crime Command"
                 economy_config = self.config.get_data()
                 reply_type = "Success" if is_success else "Failure"
-                economy_config["Commands"][income_command][reply_type]["Messages"].append(success_reply)
+                # Check to make sure that and {user_id}s supplied are valid
+                for section in reply.split(" "):
+                    if re.fullmatch(r"{[0-9]{18}}", section) is not None:
+                        if Utils.get_user_by_id(server, section[1:-1]) is None:
+                            return await Utils.simple_embed_reply(channel, "[Error]",
+                                                                  "User ID`" + section[1:-1] + "` not found.")
+                economy_config["Commands"][income_command][reply_type]["Messages"].append(reply)
                 self.config.write_data(economy_config)
-                await Utils.simple_embed_reply(message.channel, "[Success]", "Added `" +
-                                               success_reply + "` to `" + income_command + "`'s replies.")
+                await Utils.simple_embed_reply(message.channel, "[Success]", "Added `" + reply + "` to `" +
+                                               income_command + "`'s replies.")
             else:
                 await Utils.simple_embed_reply(message.channel, "[Error]", "Income command parameter not supplied.")
         else:
@@ -135,10 +159,13 @@ class Economy(Mod):
             reply = failure_messages[rng(len(failure_messages) - 1)]
         self.database.execute("UPDATE '" + server.id + "' SET balance=" + str(user_balance + balance_change) +
                               " WHERE user='" + author.id + "'")
-        await Utils.simple_embed_reply(channel, "[" + str(author) + "]", reply
-                                       .replace("{amount}", str(abs(balance_change)) +
-                                                self.config.get_data(key="Currency"))
-                                       .replace("{user}", str(author)))
+        for section in reply.split(" "):
+            if re.fullmatch(r"{[0-9]{18}}", section) is not None:
+                reply = reply.replace(section, Utils.get_user_by_id(server, section[1:-1]).mention)
+        await Utils.simple_embed_reply(channel, "[" + str(author) + "]", reply.replace("{amount}",
+                                                                                       str(abs(balance_change)) +
+                                                                                       self.config.get_data(
+                                                                                           key="Currency")))
 
     # Generates the bank DB
     def generate_db(self):
@@ -149,6 +176,88 @@ class Economy(Mod):
                         user.id) + " LIMIT 1")) == 0:
                     self.database.execute("INSERT INTO '" + server.id + "' VALUES('" + user.id + "', " +
                                           str(self.config.get_data("Starting Balance")) + ", 0)")
+
+    # Sets the range for a given income command
+    async def set_income_min_max(self, message, is_payout):
+        split_message = message.content.split(" ")
+        success_type, change_type = ("Success", "Payout") if is_payout else ("Failure", "Deduction")
+        if len(split_message) > 3:
+            income_command, minimum, maximum = split_message[1], split_message[2], split_message[3]
+            if income_command == "slut" or "work" or "crime":
+                income_command = "Slut Command" if income_command == "slut" else "Work Command" if income_command == "work" else "Crime Command"
+                if minimum.isdigit():
+                    if maximum.isdigit():
+                        # Make sure the minimum is lower than the maximum
+                        minimum, maximum = (minimum, maximum) if minimum < maximum else (maximum, minimum)
+                        economy_config = self.config.get_data()
+                        min_max_config = economy_config["Commands"][income_command][success_type][change_type]
+                        min_max_config["Min"], min_max_config["Max"] = int(minimum), int(maximum)
+                        self.config.write_data(economy_config)
+                        await Utils.simple_embed_reply(message.channel, "[Success]",
+                                                       "The `" + income_command + "` now has a " + change_type +
+                                                       " between " + minimum + " and " + maximum)
+                    else:
+                        await Utils.simple_embed_reply(message.channel, "[Error]",
+                                                       "Maximum parameter is not a digit.")
+                else:
+                    await Utils.simple_embed_reply(message.channel, "[Error]",
+                                                   "Minimum parameter is not a digit.")
+            else:
+                await Utils.simple_embed_reply(message.channel, "[Error]", "Income command parameter not supplied.")
+        else:
+            await Utils.simple_embed_reply(message.channel, "[Error]", "Insufficient parameters supplied.")
+
+    # Deletes a success/failure reply message given an ID
+    async def delete_command_reply(self, message, is_success):
+        server, channel, author = message.server, message.channel, message.author
+        split_message = message.content.split(" ")
+        if len(split_message) > 2:
+            reply_type = "Success" if is_success else "Failure"
+            income_command, index = split_message[1], split_message[2]
+            if income_command == "slut" or "work" or "crime":
+                if index.isdigit():
+                    income_command = "Slut Command" if income_command == "slut" else "Work Command" if income_command == "work" else "Crime Command"
+                    economy_config = self.config.get_data()
+                    messages = economy_config["Commands"][income_command][reply_type]["Messages"]
+                    message_to_remove = messages[int(index)]
+                    messages.remove(message_to_remove)
+                    self.config.write_data(economy_config)
+                    await Utils.simple_embed_reply(message.channel, "[Success]",
+                                                   "Deleted `" + message_to_remove + "` from `" + income_command + "`.")
+                else:
+                    await Utils.simple_embed_reply(message.channel, "[Error]", "ID parameter is not a digit.")
+            else:
+                await Utils.simple_embed_reply(message.channel, "[Error]", "Income command parameter not supplied.")
+        else:
+            await Utils.simple_embed_reply(message.channel, "[Error]", "Insufficient parameters supplied.")
+
+    # Lists all the success or failure reply messages for an income command
+    async def list_reply_commands(self, message, is_success):
+        server, channel, author = message.server, message.channel, message.author
+        split_message = message.content.split(" ")
+        if len(split_message) > 1:
+            reply_type = "Success" if is_success else "Failure"
+            income_command = split_message[1]
+            if income_command == "slut" or "work" or "crime":
+                income_command = "Slut Command" if income_command == "slut" else "Work Command" if income_command == "work" else "Crime Command"
+                current_index = 0
+                embed = discord.Embed(title="[Reply Messages]", color=discord.Color(int("0x751DDF", 16)))
+                messages = self.config.get_data(key="Commands")[income_command][reply_type]["Messages"]
+                max_number_length = len(str(len(messages)))
+                embed_text = ""
+                for reply_message in messages:
+                    current_index += 1
+                    if len(reply_message) > 50 - max_number_length:
+                        embed_text += reply_message[0: 50 - max_number_length - 3] + "...\n"
+                    else:
+                        embed_text += reply_message + "\n"
+                embed.add_field(name="ID", value=''.join([str(i) + "\n" for i in range(current_index)]), inline=True)
+                embed.add_field(name="Message", value=embed_text, inline=True)
+                await Utils.client.send_message(channel, embed=embed)
+            else:
+                await Utils.simple_embed_reply(message.channel, "[Error]", "Income command parameter not supplied.")
+        else:
+            await Utils.simple_embed_reply(message.channel, "[Error]", "Insufficient parameters supplied.")
 
 
 def roll(success_percent_chance):

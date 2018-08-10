8234c079eac9780d41fe24c3f7635c0523d32c2b
Alien <neptunesblade@gmail.com>
2018-08-10 02:22:19 -0700

    Help fix.

--- Mods/ColoredRoles/ColoredRoles.py
+++ Mods/ColoredRoles/ColoredRoles.py
@@ -5,7 +5,7 @@ import logging
 import json
 
 
-# TODO: Change from in-memory DB to on-disk DB SQL
+# TODO: Change from in-memory DB to on-disk SQL DB
 class ColoredRoles(Mod):
     def __init__(self, mod_name, embed_color):
         # General var init

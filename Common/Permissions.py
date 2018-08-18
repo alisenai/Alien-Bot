a1d9f8172cf67ba933840806de279e0c18e451a1
Alien Laptop <neptunesblade@gmail.com>
2018-08-18 00:36:23 -0700

    Permissions no longer have a DB integration due to confusion and convenience.

--- Common/Permissions.py
+++ Common/Permissions.py
@@ -10,7 +10,6 @@ def load_permissions():
     global owner_perm
     print("[Loading permissions]", end='')
     permissions_config = DataManager.get_manager("bot_config").get_data(key="Permissions")
-    DataManager.get_manager("database").execute("CREATE TABLE IF NOT EXISTS permissions(user TEXT, title TEXT)")
     for permission_name in permissions_config:
         permission_config = permissions_config[permission_name]
         permission = Permission(permission_name, permission_config["Has Permissions"],
@@ -27,36 +26,30 @@ def load_permissions():
     print("[Done]")
 
 
-# Returns a user's permission title by their ID
-def get_user_title(user_id):
-    database_manager = DataManager.get_manager("database")
-    user_title = database_manager.execute("SELECT title FROM permissions WHERE user='" + str(user_id) + "' LIMIT 1")
-    if len(user_title) == 0:
-        database_manager.execute("INSERT INTO permissions VALUES('" + str(user_id) + "', '" + default_perm.title + "')")
-        return default_perm.title
-    return user_title[0]
+# Returns a user's permission, given that user
+def get_user_permission(user):
+    for permission_name in permissions:
+        permission = permissions[permission_name]
+        if len([role for role in user.roles if int(role.id) in permission.associated_roles]):
+            return permission
+    return default_perm
 
 
+# Returns True if the given user has the given permission, else false
 def has_permission(user, minimum_permission):
-    user_title = get_user_title(user.id)
-    user_permissions = permissions[user_title]
+    user_permissions = get_user_permission(user)
     while True:
         # If the user is a bot owner, they can use any command (that is enabled) -> Return True
         if user_permissions.is_owner:
             return True
-        # If the user has a role that counts as having that permission
-        if minimum_permission in permissions:
-            if len([role for role in user.roles if int(role.id) in permissions[minimum_permission].associated_roles]) > 0:
-                return True
-        else:
-            if len([role for role in user.roles if int(role.id) in owner_perm.associated_roles]) > 0:
-                return True
         # If user title has no permissions -> Return false
         if user_permissions.has_permissions is False:
             return False
-        # If user permissions meets the minimum requirement -> Return True
-        if user_title == minimum_permission:
-            return True
+        # If the user has a role that counts as having that permission
+        if minimum_permission in permissions:
+            needed_roles = permissions[minimum_permission].associated_roles
+            if len([role for role in user.roles if int(role.id) in needed_roles]) > 0:
+                return True
         # Must be the lowest rank and thus previous checks have already determined that this user doesn't have the perms
         if user_permissions.sub_permissions is None:
             return False
